19771b4f1db67914c7ed8c7fac3b709511011af0
DanielROC-dev <dkempers@roc-dev.com>
2019-10-02 14:29:03 +0200

    Uitwerking van de exercise

--- Multiplier.php
+++ Multiplier.php
@@ -3,8 +3,7 @@ echo "> Ik ga twee getallen met elkaar vermenigvuldigen" . PHP_EOL;
 echo "> Eerste getal?" . PHP_EOL;
 $line = readline("> ");
 if (!empty($line)) {
-  readline_add_history($line);
-}
+  readline_add_history($line); }
 echo "> Tweede Getal?" . PHP_EOL;
 $bruh = readline("> ");
 if (!empty($bruh)) {
